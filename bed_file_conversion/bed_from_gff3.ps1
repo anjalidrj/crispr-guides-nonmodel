b976befc58232318b830d1421c3529591a50b836
anjalidrj <anjalidharmaraju@gmail.com>
2026-06-16 10:14:58 +0530

    Add PowerShell script to convert GFF3 to BED format
    This PowerShell script converts GFF3 gene entries to BED format. It includes steps for navigating to the folder, previewing gene names, extracting gene data, and verifying the output.

---
+++ bed_file_conversion/bed_from_gff3.ps1
@@ -0,0 +1,25 @@
+#Run one at a time in powershell(windows)
+
+#1
+cd "C:\Users\name\folder_loaction\folder_name" #copy folder containing fasta and gff3 files pathname
+
+#2
+# Preview first 3 lines matching "gene_name"
+# Change annotation.gff3 to your .gff3 file's name and "gene_name" to the name of your gene
+Get-Content annotation.gff3 | Select-String "gene_name" | Select-Object -First 3
+
+#3
+# Extract gene entry and convert to BED format
+# Change "gene_name" in the 4th, 6th and 8th line and change annptation.gff3 to your .gff3 file's name.
+Get-Content annotation.gff3 |  
+    Where-Object { $_ -notmatch "^#" } |
+    ConvertFrom-Csv -Delimiter "`t" -Header seqid,source,type,start,end,score,strand,phase,attributes |
+    Where-Object { $_.type -eq "gene" -and $_.attributes -match "Name=gene_name" } |
+    ForEach-Object {
+        "$($_.seqid)`t$([int]$_.start - 1)`t$($_.end)`tgene_name`t0`t$($_.strand)"
+    } |
+    Out-File -Encoding ascii "gene_name.bed"
+
+#4
+# Verify output, change the gene_name to match your gene's name. 
+Get-Content gene_name.bed
